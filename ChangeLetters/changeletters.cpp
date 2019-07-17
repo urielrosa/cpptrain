197b71ddf6412b742fb1f3c1ca36618f5c95e36a
Uriel A Rosa <uriel.a.rosa@gmail.com>
2019-07-16 21:13:14 -0700

    Add the top solution for Change Letters.

--- ChangeLetters/changeletters.cpp
+++ ChangeLetters/changeletters.cpp
@@ -1,35 +1,38 @@
-#include <stdio.h>
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
+string LetterChanges(string str) {
 
-string ChangeLetter(string str) {
-	const char *lut =
-		"                                 !\"#$%&'()*+,-./0123456789:;<=>?@"
-		"BCDEFGHIJKLMNOPQRSTUVWXYZA"
-		"[\\]^_ "
-		"bcdEfghIjklmnOpqrstUvwxyzA"
-		"{|}~ ";
-	size_t idx;
-
-	for (idx = 0; idx < str.length(); idx++) {
-		str[idx] = lut[str[idx]];
+	// code goes here   
+	for (int i = 0; i < str.length(); i++)
+	{
+		if ('a' <= str[i] && str[i] < 'z' || 'A' <= str[i] && str[i] < 'Z')
+		{
+			str[i]++;
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+			{
+				str[i] += 'A' - 'a';
+			}
+		}
+		else if (str[i] == 'z' || str[i] == 'Z')
+		{
+			str[i] = 'A';
+		}
 	}
-	
 	return str;
+
 }
-
-
 
 int main() {
 
 	// keep this function call here
-	// cout << LongestWord(gets(stdin));
+	//cout << LetterChanges(gets(stdin));
 	string x;
 	getline(cin, x);
-	cout << ChangeLetter(x);
+	cout << LetterChanges(x);
 	cin.get();
-
 	return 0;
+
+
 }
